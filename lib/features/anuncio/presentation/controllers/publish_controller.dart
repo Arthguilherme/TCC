b422c3ef43f97a7f50558f8a50e1c947915cdd16
Arthguilherme <arth.ssg@gmail.com>
2026-08-03 19:30:09 -0300

    feat: fazendo a parte de upload de fotos

--- lib/features/anuncio/presentation/controllers/publish_controller.dart
+++ lib/features/anuncio/presentation/controllers/publish_controller.dart
@@ -1,3 +1,4 @@
+import 'package:image_picker/image_picker.dart';
 import 'package:replaykids/features/anuncio/domain/entities/anuncio_entity.dart';
 import 'package:replaykids/features/anuncio/domain/repositories/anuncio_repository.dart';
 
@@ -9,10 +10,31 @@ class PublishController {
   String titulo = '';
   String descricao = '';
   String condicao = 'Novo';
+  List<XFile> fotos = [];
+
   String categoria = 'Brinquedos';
   String faixaEtaria = '1-2 anos';
   bool isVenda = true;
   String preco = '45,00';
+
+Future<void> adicionarFoto(ImageSource source) async {
+  if (fotos.length >= 3) return;
+
+  final picker = ImagePicker();
+  final foto = await picker.pickImage( 
+    source: source,
+    imageQuality: 80,
+    maxWidth: 1000,
+  );
+
+  if (foto != null){
+    fotos = [...fotos, foto];
+  }
+}
+
+void removerFoto(int index) {
+  fotos = [...fotos]..removeAt(index);
+}
 
   void publicar() {
     repository.publicar(
